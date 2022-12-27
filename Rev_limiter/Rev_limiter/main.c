29bb1cdf01cf3e6743764ffefbc4c851207ebf71
Evgeniy <116138692+eleprog@users.noreply.github.com>
2022-12-28 03:51:07 +0700

    Update main.c

--- Rev_limiter/Rev_limiter/main.c
+++ Rev_limiter/Rev_limiter/main.c
@@ -1,5 +1,5 @@
 ﻿//Δ
-#define F_CPU 4000000
+#define F_CPU 4400000
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
@@ -7,8 +7,8 @@
 #define RPM_OUTPUT_PIN	4
 #define SHIFT_LIGHT		3
 
-#define RPM_CUT_OFF	3500
-const uint16_t ticksCutOff = F_CPU / RPM_CUT_OFF / 30;
+#define RPM_CUT_OFF	4500
+uint16_t ticksCutOff = F_CPU / RPM_CUT_OFF * 30;
 
 uint8_t timer0Counter = 0;
 uint16_t ticksCounter = 0;
@@ -19,29 +19,24 @@ ISR(TIM0_OVF_vect) {
 }
 
 ISR(INT0_vect) {
-	asm("cli");
-	
-	ticksCounter = (timer0Counter << 8) + TCNT0;
-
-	timer0Counter = 0;
+	uint8_t tmp = TCNT0;
 	TCNT0 = 0;
 	
+	ticksCounter = (timer0Counter << 8) + tmp;
+	timer0Counter = 0;
+	
 	TIFR0 = 0;	// сброс флага прерывания timer0
-	asm("sei");
 }
 
 int main(void) {
+	
 	/* GPIO Settings */
 	DDRB	= 0b010000;
-	PORTB	= 0b000000;
-   
+	PORTB	= 0b010000;
    
 	/* INT0 Settings */
-	MCUCR |= 1<<ISC01 | 1<<ISC00;
-	//00 – низкий уровень   01 – любое изменение
-	//10 – нисходящий фронт 11 – восходящий фронт
-	
-	
+	MCUCR |= 1<<ISC01 | 1<<ISC00;	// 10 – нисходящий фронт 11 – восходящий фронт
+	GIMSK = 1<<INT0;				// разрешение прерываний INT0
 	
 	/* TIMER0 Settings */
 	TCCR0B = (0<<CS02)|(0<<CS01)|(1<<CS00);	// установка предделителя (0) таймера 0
@@ -51,7 +46,7 @@ int main(void) {
 	asm("sei");
     while (1) {
 		if(ticksCounter) {
-			if(ticksCounter < ticksCutOff)
+			if(ticksCounter > ticksCutOff)
 				PORTB |= 1<<RPM_OUTPUT_PIN;
 			else
 				PORTB &= ~(1<<RPM_OUTPUT_PIN);
