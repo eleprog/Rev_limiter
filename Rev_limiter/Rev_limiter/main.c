fdf220093b4a603b72fe682fc47e5568023dec4e
Evgeniy <116138692+eleprog@users.noreply.github.com>
2023-10-11 00:28:06 +0700

    stable version 2.0

--- Rev_limiter/Rev_limiter/main.c
+++ Rev_limiter/Rev_limiter/main.c
@@ -1,42 +1,26 @@
-﻿//Δ
-#define F_CPU 4400000
+﻿#define F_CPU 4500000 //Δ
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
+#define RPM_CUT_OFF		4000
+
 #define RPM_INPUT_PIN	1
 #define RPM_OUTPUT_PIN	4
-
 #define SHIFT_LIGHT_PIN	3
 #define BUTTON_PIN		2
 
-#define RPM_CUT_OFF		4000
-const uint16_t ticksCutOff = F_CPU / RPM_CUT_OFF * 30;
+const uint16_t ticksPerRotationCutOff = F_CPU / RPM_CUT_OFF * 15;
 volatile uint16_t ticksCounter = 0;
 
-#define SHIFT_LIGHT_OFF_DELAY 300	// (15мс - 3800мс)
-const uint16_t shiftLightOff = F_CPU / 1000 * SHIFT_LIGHT_OFF_DELAY / 256;
-volatile uint16_t shiftLightCounter = 0;
+volatile uint8_t shiftLightCounter = 0;
+volatile uint8_t timer0Counter = 0;
 
-#define TICKS_CUT_OFF_STEP 300;
-
-volatile uint8_t mapSparkPointer = 7;
-uint8_t mapSpark[] = {
-	0b0000000,
-	0b0100000,
-	0b0100010,
-	0b0110010,
-	0b0110110,
-	0b1110110,
-	0b1111110,
-	0b1111111};
-
-
-volatile static uint8_t timer0Counter = 0;
+volatile uint8_t mapSpark = 0b1111111;
 
 ISR(TIM0_OVF_vect) {
-	if(timer0Counter < 255)
+	if(timer0Counter < 0x3F)
 		timer0Counter++;
-		
+			
 	if(shiftLightCounter)
 		shiftLightCounter--;
 } 
@@ -44,27 +28,25 @@ ISR(TIM0_OVF_vect) {
 ISR(INT0_vect) {
 	static uint8_t mapMask = 1;
 	
-	if(PINB & 1<<RPM_INPUT_PIN) 
+	if(PINB & 1<<RPM_INPUT_PIN)
 		PORTB &= ~(1<<RPM_OUTPUT_PIN);
 				
 	else {
 		uint8_t tmp = TCNT0;
 		TCNT0 = 0;
 		
-		if(mapSpark[mapSparkPointer] & mapMask)
+		if(mapSpark & mapMask)
 			PORTB |= 1<<RPM_OUTPUT_PIN;
 		
 		mapMask <<= 1;
 		if(mapMask == 0b10000000)
-		mapMask = 1;
+			mapMask = 1;
 		
 		ticksCounter = (timer0Counter << 8) + tmp;
 		
 		timer0Counter = 0;
-		TIFR0 |= 1<<TOV0;	// сброс флага OVF timer0
+		TIFR0 = 0;	// reset flag OVF timer0
 	}
-	
-		
 }
 
 int main(void) {
@@ -77,51 +59,46 @@ int main(void) {
 	GIMSK	= 1<<INT0;		// разрешение прерываний INT0
 
 	/* TIMER0 Settings */
-	TCCR0B	= 0b001<<CS00;	// установка предделителя (0) таймера 0
+	TCCR0B	= 0b010<<CS00;	// установка предделителя (8) таймера 0
 	TIMSK0	= 1<<TOIE0;		// разрешение прерываний по совпадению
 	TCNT0	= 0;			// обнуление счетного регистра таймера 0
-	
 	asm("sei");
 	
-	
-	static uint16_t ticksArr[4] = {0xFFFF,0xFFFF,0xFFFF,0xFFFF};
-	static uint32_t	ticksPerRotation = 0x3FFFC;
-	static uint8_t ticksArrCounter = 0;
+	static uint16_t	ticksPerRotation = 0xFFFC;
+	static uint16_t ticksArr[4] = {0x3FFF, 0x3FFF, 0x3FFF, 0x3FFF};
+	static uint8_t	ticksArrCounter = 0;
 	
     while (1) {
 		if(ticksCounter) {
-			if(++ticksArrCounter > 3)
-				ticksArrCounter = 0;
-				
 			ticksPerRotation -= ticksArr[ticksArrCounter];
 			ticksPerRotation += ticksCounter;
 			ticksArr[ticksArrCounter] = ticksCounter;
-					
-			uint16_t ticks = ticksPerRotation / 4;
-			if(ticks < ticksCutOff) {
-				uint16_t delta = ticksCutOff - ticks;
+			
+			if(++ticksArrCounter > 3)
+				ticksArrCounter = 0;
+			
+			if(ticksPerRotation > ticksPerRotationCutOff) {
+				mapSpark = 0b1111111;
+			}
+			else {
+				uint16_t delta = ticksPerRotationCutOff - ticksPerRotation;
 				
-				if(delta < 82)			mapSparkPointer = 6;
-				else if(delta < 246)	mapSparkPointer = 5;
-				else if(delta < 488)	mapSparkPointer = 4;
-				else if(delta < 805)	mapSparkPointer = 3;
-				else if(delta < 1193)	mapSparkPointer = 2;
-				else if(delta < 1571)	mapSparkPointer = 1;
-				else					mapSparkPointer = 0;
+				if(delta < 42)			mapSpark = 0b1111110;
+				else if(delta < 126)	mapSpark = 0b1110110;
+				else if(delta < 249)	mapSpark = 0b0110110;
+				else if(delta < 412)	mapSpark = 0b0110010;
+				else if(delta < 610)	mapSpark = 0b0100010;
+				else if(delta < 804)	mapSpark = 0b0100000;
+				else					mapSpark = 0b0000000;
 				
-				shiftLightCounter = shiftLightOff;
+				shiftLightCounter = 0xFF;
 				PORTB |= 1<<SHIFT_LIGHT_PIN;
 			}
-			else
-				mapSparkPointer = 7;
-			
 			ticksCounter = 0;
 		}
 
 		if(!shiftLightCounter)
 			PORTB &= ~(1<<SHIFT_LIGHT_PIN);
 			
-			
 	}
-}	
-		
+}
